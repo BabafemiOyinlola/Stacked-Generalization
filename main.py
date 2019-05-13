72f4402fcaba81dd6ffb7fdba910c20f6fb536d4
Oyinlola <babsoyinlola@yahoo.com>
2019-05-13 18:39:03 +0100

    Noise fixed. Different levels of noise can be used

--- main.py
+++ main.py
@@ -2,24 +2,16 @@ import math
 import random
 import numpy as np
 import pandas as pd
-from sklearn.svm import SVC
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_predict
-from sklearn.model_selection import StratifiedKFold, KFold
-from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_breast_cancer
 
+from experiments import cross_validate_clfs, cross_validate_clfs_noise
 from stacked_generalization import StackedGeneralization
-
-scaler = StandardScaler()
-label_encoder = LabelEncoder()
 
 def split_data(filepath, lbl_pos=-1):
     data = pd.read_csv(filepath, header=None)
@@ -37,196 +29,32 @@ def split_data(filepath, lbl_pos=-1):
 
     return (X_train, X_test, y_train, y_test) 
 
-def add_noise(data, filepath=None, encode_label=False):
-    mu, sigma = 0, 0.1 
-    shape = [i for i in data.shape]
-    noise = np.random.normal(mu, sigma, shape)
-    data_copy = data.copy()
-    if encode_label:
-        data_copy.iloc[:,-1] = label_encoder.fit_transform(data_copy.iloc[:,-1])
-
-    clean = np.array(data_copy)  
-    noise = clean + noise
-
-    noise = np.delete(noise, -1, axis=1)
-    noise = np.column_stack((noise, np.array(data)[:, -1]))
-    
-    noise = pd.DataFrame(noise)
-    if filepath is not None:
-        noise.to_csv(filepath, header=None, index=False)
-        
-    return noise
-        
-def cross_validate_clfs(X, y, classifiers_cv, classifiers_cv_names, meta_clf, folds=5):
-    clfs_accuracies = []
-
-    skf = StratifiedKFold(n_splits=folds, shuffle=True)
-    for i in range(len(classifiers_cv)):
-        acc = []
-        for train_index, test_index in skf.split(X, y):
-            X_train, X_test = X[train_index], X[test_index]
-            y_train, y_test = y[train_index], y[test_index]
-            score = 0
-            pred = None
-            clf = None
-            if i == len(classifiers_cv) - 1:
-                if hasattr(classifiers_cv[i](classifiers_cv[0:i-1], meta_clf), 'clf_name'):
-                    clf = classifiers_cv[i](classifiers_cv[0:i-1], meta_clf)
-                else:
-                    clf = classifiers_cv[i]()
-                clf.fit(X_train, y_train)
-                pred = clf.predict(X_test, prob=True)  
-            else:
-                if hasattr(classifiers_cv[i](classifiers_cv[0:i], meta_clf), 'clf_name'):
-                    clf = classifiers_cv[i](classifiers_cv[0:i], meta_clf)
-                else:
-                    clf = classifiers_cv[i]()
-                clf.fit(X_train, y_train)
-                pred = clf.predict(X_test)
-            score = accuracy_score(y_test, pred)
-            acc.append(score)
-        clfs_accuracies.append(np.mean(acc))
-
-    combine = zip(classifiers_cv_names, clfs_accuracies)
-    return combine
-
-def cross_validate_clfs_noise(X, y, X_noise, y_noise, classifiers_cv, classifiers_cv_names, meta_clf, folds=5):
-    clfs_accuracies = []
-
-    skf = StratifiedKFold(n_splits=folds, shuffle=True)
-    for i in range(len(classifiers_cv)):
-        acc = []
-        for train_index, test_index in skf.split(X, y):
-            X_train, X_test = X_noise[train_index], X[test_index]
-            y_train, y_test = y_noise[train_index], y[test_index]
-            score = 0
-            pred = None
-            clf = None
-            if i == len(classifiers_cv) - 1:
-                if hasattr(classifiers_cv[i](classifiers_cv[0:i-1], meta_clf), 'clf_name'):
-                    clf = classifiers_cv[i](classifiers_cv[0:i-1], meta_clf)
-                else:
-                    clf = classifiers_cv[i]()
-                clf.fit(X_train, y_train)
-                pred = clf.predict(X_test, prob=True)  
-            else:
-                if hasattr(classifiers_cv[i](classifiers_cv[0:i], meta_clf), 'clf_name'):
-                    clf = classifiers_cv[i](classifiers_cv[0:i], meta_clf)
-                else:
-                    clf = classifiers_cv[i]()
-                clf.fit(X_train, y_train)
-                pred = clf.predict(X_test)
-            score = accuracy_score(y_test, pred)
-            acc.append(score)
-        clfs_accuracies.append(np.mean(acc))
-
-    combine = zip(classifiers_cv_names, clfs_accuracies)
-    return combine
-
-
 if __name__ == "__main__":
-    # X_train, X_test, y_train, y_test = split_data('data/waveform/waveform.txt', -1)
-
-    knn, dt, svm, nb = KNeighborsClassifier(), DecisionTreeClassifier(), SVC(), GaussianNB()
-    lr = LogisticRegression()
+    knn, dt, svm, lr  = KNeighborsClassifier(), DecisionTreeClassifier(), SVC(), LogisticRegression()
     classifiers = [KNeighborsClassifier, LogisticRegression, DecisionTreeClassifier]
 
-    # data = pd.read_csv('data/waveform/waveform.txt', header=None)
+    data = pd.read_csv('data/waveform/waveform.txt', header=None)
     # data = pd.read_csv('data/glass/glass.csv', header=None)
-    data = pd.read_csv('data/ionosphere/ionosphere.csv', header=None)
+
+    # data = pd.read_csv('data/ionosphere/ionosphere.csv', header=None)
     # data = pd.read_csv('data/breast_cancer/breast_cancer.csv', header=None)
-    noise_data = add_noise(data, encode_label=True)
-    df_len = int(len(noise_data)/4)
-    # noise_to_add = noise_data.iloc[0:df_len]
-    # noise_left = noise_data.iloc[df_len:]
 
-    data_copy = data.copy()
-    noise_copy  = noise_data.copy()
+    data = data.sample(frac=1).reset_index(drop=True) #shuffle
 
-    data_copy.iloc[0:df_len] = noise_data.iloc[0:df_len]
-    data_copy.iloc[df_len:] = data.iloc[df_len:]
-    data_copy = data_copy.sample(frac=1).reset_index(drop=True) #shuffle
-
-    noise_copy.iloc[0:df_len] = data.iloc[0:df_len]
-    noise_copy.iloc[df_len:] = noise_data.iloc[df_len:]
-    noise_copy = noise_copy.sample(frac=1).reset_index(drop=True)
-
-    data_copy = np.array(data_copy)
-    y = data_copy[:, -1]
-    y = label_encoder.fit_transform(y)
-
-    X = data_copy[:, 0:data.shape[1]-1]
-    X = scaler.fit_transform(X)
-
-    noise_copy = np.array(noise_copy)
-    y_noise = noise_copy[:, -1]
-    y_noise = label_encoder.fit_transform(y)
-
-    X_noise = noise_copy[:, 0:noise_copy.shape[1]-1]
-    X_noise = scaler.fit_transform(X_noise)
-
-    stk = StackedGeneralization(classifiers, svm)
-
-    # classifiers_cv = [knn, lr, dt, stk, stk]
-    classifiers_cv = [KNeighborsClassifier, LogisticRegression, DecisionTreeClassifier, StackedGeneralization, StackedGeneralization]
+    classifiers_cv = [KNeighborsClassifier, LogisticRegression, DecisionTreeClassifier, StackedGeneralization, StackedGeneralization]   
     classifiers_cv_names = ["knn", "lr", "dt", "stacked", "stacked_prob"]
 
-    cv_results = cross_validate_clfs(X, y, classifiers_cv, classifiers_cv_names, meta_clf=SVC)
-    cv_results_noise = cross_validate_clfs_noise(X, y, X_noise, y_noise, classifiers_cv, classifiers_cv_names, meta_clf=SVC)
-
-
-    print("CROSS VALIDATION NOISELESS \n")
-
+    cv_results = cross_validate_clfs(data, classifiers_cv, classifiers_cv_names, meta_clf=SVC)
+    print("\nCROSS VALIDATION NOISELESS \n")
     for clf, acc in cv_results:
-        print(clf + " : " + str(round(acc, 2)) + " \t\t\tError: " + str(round(1 - acc, 2)))
-
-    print("\n\nCROSS VALIDATION NOISE \n")
-
-    for clf, acc in cv_results_noise:
-        print(clf + " : " + str(round(acc, 2)) + " \t\t\tError: " + str(round(1 - acc, 2)))
+        print(clf + " : " + str(round(acc, 3)) + " \t\t\tError: " + str(round(1 - acc, 3)))
+    
+    print("\nCROSS VALIDATION NOISE \n")
+    for i in range(20, 80, 10):
+        print("Noise level: ", i)
+        cv_results_noise = cross_validate_clfs_noise(data, classifiers_cv, classifiers_cv_names, meta_clf=SVC, level=i, encode=False)
+        print("\n")
+        for clf, acc in cv_results_noise:
+            print(clf + " : " + str(round(acc, 3)) + " \t\t\tError: " + str(round(1 - acc, 3)))
 
     print("\nDone")
-
-
-# for i in range(10):
-    # dt = load_breast_cancer()
-    # X = dt.data
-    # y = dt.target
-
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-
-    # stacked = StackedGeneralization(classifiers, lr)
-    # stacked.fit(X_train, y_train)
-    # stk_pred = stacked.predict(X_test, cv=5)
-
-    # stacked_prob = StackedGeneralization(classifiers, lr)
-    # stacked_prob.fit(X_train, y_train)
-    # stk_pred_prob = stacked_prob.predict(X_test, cv=5, prob=True)
-
-    # KNN = knn.fit(X_train, y_train)
-    # knn_pred = KNN.predict(X_test)
-
-    # SVM  = svm.fit(X_train, y_train)
-    # svm_pred = SVM.predict(X_test)
-
-    # # NB = nb.fit(X_train, y_train)
-    # # nb_pred = NB.predict(X_test)
-
-    # DT = dt.fit(X_train, y_train)
-    # dt_pred = DT.predict(X_test)
-
-    # stk_acc = round(accuracy_score(y_test, stk_pred), 2)
-    # stk_acc_prob = round(accuracy_score(y_test, stk_pred_prob), 2)
-    # knn_acc = round(accuracy_score(y_test, knn_pred), 2)
-    # svm_acc = round(accuracy_score(y_test, svm_pred), 2)
-    # # nb_acc = round(accuracy_score(y_test, nb_pred), 2)
-    # dt_acc = round(accuracy_score(y_test, dt_pred), 2)
-
-    # print("\n\n")
-    # print("stk_acc: ", stk_acc)
-    # print("stk_acc_prob: ", stk_acc_prob)
-    # print("knn_acc: ", knn_acc)
-    # print("svm_acc: ", svm_acc)
-    # # print("nb_acc: ", nb_acc)
-    # print("dt_acc: ", dt_acc)
--- stacked_generalization.py
+++ stacked_generalization.py
@@ -2,26 +2,27 @@ import math
 import random
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_breast_cancer
-
 
 class StackedGeneralization():
+    '''
+    Stacked Generalization ensembles:
+    1) Using predictions of each base classifier to train the meta classifier
+    2) Using predicted probabilities of labels by base classifiers to train the meta classifer
+    '''
     def __init__(self, classifiers, meta_clf):
         self.classifiers = classifiers
         self.meta_clf = meta_clf
-        self.clf_name = "STG"
+        self.clf_name = "STG" #this was defined to identify classifier when using cv
     
     def fit(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
 
-    def base_classifiers(self, X_test, cv=5): #MAKE METHOD PRIVATE
+    def __base_classifiers(self, X_test, cv=5):
+        '''
         #1) Obtain the prediction for each classifier using a cross validation
         #2) Fit all the training items on each classifier
+        '''
 
         #1) cv for each classifier
         feat_train_set = self.X_train
@@ -43,12 +44,8 @@ class StackedGeneralization():
             start_fold = 0
             end_fold = fold_size
 
-            # split into folds then pick
+            # split into folds
             for i in range(cv):
-                #split into folds
-                # print("Start fold: ", start_fold)
-                # print("End fold: ", end_fold)
-                
                 if i == (cv - 1):
                     test_fold = combined_set_copy[start_fold:]
                     train_fold = combined_set_copy.drop(combined_set_copy.index[start_fold: ])
@@ -68,16 +65,13 @@ class StackedGeneralization():
                 if i == (cv - 1):
                     rowIndex = combined_set.index[start_fold: (start_fold +len(test_fold))]
                     combined_set.loc[rowIndex, 'pred'] = pred
-                    # print(rowIndex)
                 else:
                     rowIndex = combined_set.index[start_fold: end_fold]
                     combined_set.loc[rowIndex, 'pred'] = pred
-                    # print(rowIndex)
             
                 start_fold = end_fold
                 end_fold = end_fold + fold_size
                 pred = combined_set['pred']
-                # pred = [[i] for i in pred]
             all_predictions.append(np.array(pred))
 
         stacked_cv_predictions = None
@@ -105,29 +99,30 @@ class StackedGeneralization():
 
         return (stacked_cv_predictions, stacked_test_predictions)
 
-    #RE-WRITE
-    def base_classifiers_prob(self, X_test, cv=5): #MAKE METHOD PRIVATE
-        #1) Obtain the prediction for each classifier using a cross validation
+    def __base_classifiers_prob(self, X_test, cv=5):
+        '''
+        #1) Obtain the probabilities for predicting a particular class for each classifier using a cross validation
         #2) Fit all the training items on each classifier
+        '''
 
         #1) cv for each classifier
         feat_train_set = self.X_train
         lbl_trian_set = self.y_train
         pred = [None] * len(feat_train_set)
+
+        #combine features and labels to numpy array
         stack = np.column_stack((feat_train_set, lbl_trian_set))
         stack = np.column_stack((stack, pred))
 
-        #TO DO: SHUFFLE DATA
         combined_set = pd.DataFrame(pd.DataFrame(stack))
         combined_set=combined_set.rename(columns = {len(combined_set.columns) - 1:'pred'})
         combined_set=combined_set.rename(columns = {len(combined_set.columns) - 2:'y_train'})
-        # combined_set = combined_set.sample(frac=1).reset_index(drop=True)
 
         combined_set_copy = combined_set
 
         all_predictions = []
         all_preds = np.array([])
-        fold_size = int(len(feat_train_set)/cv)
+        fold_size = int(len(feat_train_set)/cv) 
 
         for classifier in self.classifiers:
             start_fold = 0
@@ -150,16 +145,13 @@ class StackedGeneralization():
                 
                 clf = classifier().fit(X_train_fold, y_train_fold)
                 pred = [np.array(i).tolist() for i in clf.predict_proba(X_test_fold)]
-                # pred = [i.tolist() for i in clf.predict_proba(X_test_fold)]
 
                 if i == (cv - 1):
                     rowIndex = combined_set.index[start_fold: (start_fold +len(test_fold))]
                     combined_set.loc[rowIndex, 'pred'] = pred
-                    # print(rowIndex)
                 else:
                     rowIndex = combined_set.index[start_fold: end_fold]
                     combined_set.loc[rowIndex, 'pred'] = pred
-                    # print(rowIndex)
             
                 start_fold = end_fold
                 end_fold = end_fold + fold_size
@@ -171,48 +163,26 @@ class StackedGeneralization():
             else:
                 all_preds = pred
 
-        # flattened = None
-        # for i in range(len(all_preds)):
-        #     item = all_preds[i]
-        #     flat = []
-        #     for i in item:
-        #         flat.extend(i)
-        #     # flattened.append(np.array(flat).reshape(len(flat),-1))
-        #     flat = np.array(flat).reshape(-1, len(flat))
-        #     if flattened is None:
-        #         flattened = flat
-        #     else:
-        #         flattened = np.append(flattened, flat, axis = 0)
-        #     # flattened.append(np.array(flat).reshape(-1, len(flat)))
-
-        # flattened = np.array(flattened)
         flattened = np.mean(np.array(all_predictions), axis=0)
-        stacked_cv_predictions = flattened #train meta-classifier with stacked probabilities
+        stacked_cv_predictions = flattened #output to used in training meta-classifier
 
         #2) train classifiers on full training set
         full_predictions= []
         for classifier in self.classifiers:
-            clf = classifier().fit(self.X_train, self.y_train) #classifiers trained on full training set
+            clf = classifier().fit(self.X_train, self.y_train) #classifiers trained newly on full training set
             pred = clf.predict_proba(X_test) #predict labels of the test set
             full_predictions.append(pred)
 
-        # stacked_test_predictions = None 
-        stacked_test_predictions = np.mean(full_predictions, axis=0)
-        # for i in range(len(full_predictions)):
-        #     if i == len(full_predictions) - 1: break
-        #     if i == 0:
-        #         stacked_test_predictions = np.column_stack((full_predictions[i], full_predictions[i + 1]))
-        #     else:
-        #         stacked_test_predictions = np.column_stack((stacked_test_predictions, full_predictions[i+1]))
-
+        stacked_test_predictions = np.mean(full_predictions, axis=0) #new test set used to test classifier
+        
         return (stacked_cv_predictions, stacked_test_predictions)
     
     def predict(self, X_test, cv_=5, prob=False):
         #train the meta classifer with the results of the predictions from the base classifers using a cross validated method
         if prob:
-            predictions = self.base_classifiers_prob(X_test, cv=cv_)
+            predictions = self.__base_classifiers_prob(X_test, cv=cv_)
         else:
-            predictions = self.base_classifiers(X_test, cv=cv_)
+            predictions = self.__base_classifiers(X_test, cv=cv_)
 
         stacked_cv_predictions = predictions[0]
         stacked_test_predictions = predictions[1]
@@ -222,15 +192,3 @@ class StackedGeneralization():
         final_pred = clf.predict(stacked_test_predictions)
 
         return final_pred
-
-    def accuracy(self, y_true, y_test):
-        correct = []
-        for i in range(len(y_test) - 1):
-            if y_test[i] == y_true[i]:
-                correct.append(1)
-            else:
-                correct.append(0)
-        match = sum(correct)
-        acc = (match / len(y_test)) * 100
-        acc = round(acc, 2)
-        return acc
